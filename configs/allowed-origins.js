07532107c52354778142cfcb5894a59d5d311415
Vulpes231 <whykayshot@gmail.com>
2024-08-17 08:56:45 +0100

    cor

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3600",
   "http://localhost:5173",
   "https://lustrous-torrone-e7404f.netlify.app",
+  "https://66bf9bb54e548985218ced7b--lustrous-torrone-e7404f.netlify.app",
 ];
 
 module.exports = allowedOrigins;
