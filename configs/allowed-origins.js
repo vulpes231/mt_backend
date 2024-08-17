bae526941ae46dd28452c672db0a20d9c961fde2
Vulpes231 <whykayshot@gmail.com>
2024-08-17 07:34:43 +0100

    cors

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3600",
   "http://localhost:5173",
+  "https://lustrous-torrone-e7404f.netlify.app",
 ];
 
 module.exports = allowedOrigins;
