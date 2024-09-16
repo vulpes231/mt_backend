6e8eacdc54c5faa094615cd339957a34bc1517a8
Vulpes231 <whykayshot@gmail.com>
2024-09-16 13:36:40 +0100

    cors

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -1,9 +1,8 @@
 const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3600",
-  "http://localhost:5173",
-  "https://lustrous-torrone-e7404f.netlify.app",
-  "https://66bf9bb54e548985218ced7b--lustrous-torrone-e7404f.netlify.app",
+  "https://pinnaclemeta.com",
+  "https://metro-m9ur.onrender.com",
 ];
 
 module.exports = allowedOrigins;
