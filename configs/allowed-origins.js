9cf257cd0638c6079a58776e0058d589c58b0997
Vulpes231 <whykayshot@gmail.com>
2025-09-20 10:29:37 +0100

    init

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:4000",
-  "https://pinnaclemeta.com",
-  "https://metro-m9ur.onrender.com",
+	"http://localhost:5173",
+	"http://localhost:4000",
+	"https://pinnaclemeta.com",
+	"https://metro-m9ur.onrender.com",
+	"https://lustrous-torrone-e7404f.netlify.app",
 ];
 
 module.exports = allowedOrigins;
